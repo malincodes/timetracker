bbbd62513540edf596ed7248d10a33bbf5fc6dc7
malincodes <malin.inzinger@gmail.com>
2023-04-07 11:36:54 +0200

    more cleanup of old code

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-// PART 1: 
-// Step 1: get all the bubbles - refer to HTML elements in this doc
-//const bubbles = document.querySelectorAll('.bubble');
-
-// Step 2: for each bubble, attach an on-click event listener that executes a function (loop throouogh it)
-// bubbles.forEach(function(bubble) {
-    //bubble.addEventListener('click', toggleButton)
-// });
-
-// Step 3: write the function that when you click the bubble, it removes the play class and adds a pause class, or vice versa
+//Main framework
 const startstamp = {};
 const endstamp = {};
 const totalstamp = {};
 
+//toggles button from play to pause and back
 function toggleButton(ev){
     const currentbubble = ev.currentTarget;
     const currentproject = currentbubble.getElementsByClassName('project-name')[0].innerHTML;
@@ -32,23 +24,6 @@ function toggleButton(ev){
     }
 
 };
-
-// PART 2: Track time
-// Step 1: log time of click and then minus time of click again (comes from eventlistener info)
-// Step 2: do all of that in the togglebutton function, get the children from the currentbubble (project name is one of them)
-// when hit play, save the current time on the project name (in a dict) 
-// when hit pause, calculate time difference and save over infoo on project name dict
-// one dict for start time, one dict with end time, one dict where I collect all the time - key is always project name
-// save the times in a dictionary and add new times to it
-// at hover, calculate time difference and show next to project name
-
-
-//PART 3: Add time difference to project name in HTML
-//PART 4: how to add project names/bubbles dynamically? 
-//Step 1: dynamically add the bubble div to the main container div based on an eventlistener (plus button click)
-//Step 2: make a plus button and add an event listener - ffunction is add the bubble div
-//Step 3: dynamically add the project name with input when plus button is clicked - make sure that the project is an ID not a name, so I can change names later
-//append child element to main container
 
 //function to add new bubbles to main container
 document.getElementById("add-button").addEventListener('click', addProject);
@@ -71,15 +46,4 @@ function addProject() {
         //listen for click in newly created bubble
         bubble.addEventListener('click', toggleButton);
     }
-
-// find element with id  and put eventlistener on that
-//function to remove bubbles from main container - button is within bubble
-
 }
-
-
-//things I need in my HTML: add button with text input, remove button, change project name button which would be in the bubbles div
-//remove button
-//put in electron
-// event listener enter statt click - mit click!
-
